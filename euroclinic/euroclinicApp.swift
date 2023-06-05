224225fb42c5523bf5258bbca69190b6328ade84
Milo4uk <wlir0tn@gmail.com>
2023-06-05 13:08:59 +0300

    Initial Commit

---
+++ euroclinic/euroclinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  euroclinicApp.swift
+//  euroclinic
+//
+//  Created by Mila ✨ on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct euroclinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
